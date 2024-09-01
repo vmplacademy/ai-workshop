13b88f2999df19785e54584b73a5ef87d9658aec
Tomasz Kluza <tomasz.kluza@picard.de>
2024-09-01 20:27:03 +0300

    feature: Initial db structure removed.

--- backend/src/main/resources/db/migration/V1__init_db.sql
+++
@@ -1,6 +0,0 @@
--- backend/src/main/resources/db/migration/V1__init_db.sql
-CREATE DATABASE todolist_db;
-
-CREATE USER main_user WITH ENCRYPTED PASSWORD 'main_user_pasword';
-
-GRANT ALL PRIVILEGES ON DATABASE todolist_db TO main_user;
